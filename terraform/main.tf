e3684e3b479f57cda2c0963389e4b5535a0d9d71
allig4t0r <allig4t0r@gmail.com>
2025-12-09 21:02:46 +0300

    total refactor + add multiple disks feature (#1)
    * Testing GPG signing
    
    * Testing GPG signing
    
    * total refactor + multiple disks
    
    * cosmetics

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
 module "proxmox_vms" {
-  source                       = "./modules/cloud-init"
-  proxmox_host                 = var.proxmox_host
-#   proxmox_host_ipv4_addrs      = var.proxmox_host_ipv4_addrs
-  proxmox_user                 = var.proxmox_user
-  proxmox_user_private_key     = file("~/.ssh/id_ed25519")
-  root_ssh_pub_key             = file("~/.ssh/id_ed25519.pub")
-  default_non_root_user        = var.default_non_root_user
+  source                          = "./modules/cloud-init"
+  proxmox_host                    = var.proxmox_host
+  # proxmox_host_ipv4_addrs         = var.proxmox_host_ipv4_addrs
+  proxmox_user                    = var.proxmox_user
+  proxmox_user_private_key        = file("~/.ssh/id_ed25519")
+  root_ssh_pub_key                = file("~/.ssh/id_ed25519.pub")
+  default_non_root_user           = var.default_non_root_user
   default_non_root_user_hashed_pw = var.default_non_root_user_hashed_pw
-  vms                          = local.vms_with_defaults
+  vms                             = local.vms_with_defaults
+  extra_disks                     = local.extra_disks
 }
--- terraform/modules/cloud-init/provider.tf
+++ terraform/modules/cloud-init/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "bpg/proxmox"
-      version = "0.86.0" //November 2025
+      source  = "bpg/proxmox"
+      version = "0.89.1"
     }
   }
 }
