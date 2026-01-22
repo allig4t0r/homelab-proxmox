b654ec29ef3f58542e822b792dd4c2b21911a07b
allig4t0r <allig4t0r@gmail.com>
2026-01-22 13:02:41 +0300

    total refactor of logging, lock, cleanup, new dl loging with tmp file

--- pve_templater.sh
+++ pve_templater.sh
@@ -11,12 +11,15 @@
 # wget to download the images
 # xz to unzip talos images
 # curl and jq for getting the metadata, latest version info, etc
+# lsof for checking lock file descriptors
 
 set -euo pipefail
+# set -o errtrace
+# set -o functrace
 
 LOG_FILE="/var/log/pve_templater.log"
+DEBUG_ENABLED=0  #TODO get value from -v/--verbose?
 LOCK_FILE="/tmp/pve_templater.lock"
-TMP_LOG=$(mktemp -t pve_templater_logXXXXXXXXXX)
 MAX_LOG_SIZE=1048576   # 1MB
 CACHE_DIR="/var/lib/vz/template/images"
 STORAGE="nvme"
@@ -34,7 +37,10 @@ CORES=2
 #             - siderolabs/util-linux-tools
 TALOS_SCHEMATIC_ID="46d4c1f71ea8a0d5deeb85c27e5f4e9479ae592a532de2856f96926949294324"
 
+############################# SYSTEM ##########################################
+
 display_usage() {
+  log_debug "Displaying usage"
   cat <<EOF
 Usage: pve_templater.sh <ubuntu|talos|flatcar> <vm id> [--resize <size>]
 
@@ -43,27 +49,49 @@ EOF
 }
 
 require_bin() {
-    if ! command -v "$1" >/dev/null 2>&1; then
-        if [ "$1" = "virt-customize" ]; then
-            echo "[ERROR] libguestfs-tools required for injecting qemu-guest-agent." >&2
-        elif [ "$1" = "qm" ]; then
-            echo "[ERROR] Are we on Proxmox host? qm not found" >&2
+    local bin="$1"
+    log_debug "Checking if ${bin} is installed and working"
+    if ! command -v "$bin" >/dev/null 2>&1; then
+        if [ "$bin" = "virt-customize" ]; then
+            log_crit "libguestfs-tools required for injecting qemu-guest-agent." >&2
+        elif [ "$bin" = "qm" ]; then
+            log_crit "Are we on Proxmox host? qm not found" >&2
         else
-            echo "[ERROR] Required binary '$1' not installed." >&2
+            log_crit "Required binary ${bin} not installed." >&2
         fi
         exit 1
     fi
+    log_debug "${bin} is installed and working"
 }
 
-###################################PVE#########################################
+get_lock() {
+    require_bin lsof
+    log_debug "Trying to acquire the lock file"
+    exec 9>"$LOCK_FILE" || exit 1
+    flock -n 9 || {
+        log_crit "Another pve_templater instance is running"
+        log_info "Lock file: $LOCK_FILE"
+        log_info "Processes holding or referencing the lock:"
+        # lsof output is multiline → preserve formatting
+        while IFS= read -r line; do
+            log_info "$line"
+        done < <(lsof "$LOCK_FILE" 2>/dev/null || true)
+        exit 1
+    }
+    log_debug "Lock file was acquired"
+}
+
+################################## PVE ########################################
 
 check_vm() {
     require_bin qm
     local vmid="$1"
+    log_debug "Checking if VM ${vmid} exists"
     if qm status "$vmid" &>/dev/null; then
-        echo "[WARN] VM ${vmid} already exists. Removing old VM..."
+        log_warn "VM ${vmid} already exists. Removing old VM..."
         qm stop "$vmid" --skiplock 2>/dev/null || true
         qm destroy "$vmid" --purge 2>/dev/null || true
+        log_info "VM ${vmid} was successfully removed"
     fi
 }
 
@@ -72,7 +100,7 @@ create_vm() {
     local vmid="$1"
     local name="$2"
     check_vm $vmid
-    echo "Creating VM $vmid ($name)"
+    log_info "Creating VM ${vmid} (${name})..."
     qm create "$vmid" \
         --name "$name" \
         --ostype l26 \
@@ -86,17 +114,20 @@ create_vm() {
         --cpu host \
         --scsihw virtio-scsi-single \
         --tags template
+    log_info "Successfully created new VM"
 }
 
 import_disk() {
     require_bin qm
     local vmid="$1"
     local image="$2"
-    echo "[INFO] Importing disk..."
+    log_info "Importing disk..."
     qm importdisk "$vmid" "$image" "$STORAGE"
+    log_debug "Initializing disk on SCSI1..."
     qm set "$vmid" \
         --scsi1 "${STORAGE}:vm-${vmid}-disk-1,iothread=1,discard=on,ssd=1" \
         --boot order=scsi1
+    log_info "Disk was successfully imported"
 }
 
 get_template_info() {
@@ -104,12 +135,15 @@ get_template_info() {
     local mode="$1"
     local vmid="$2"
     local result
+    log_info "Getting template information for VM ${vmid}..."
     if qm config "$vmid" >/dev/null 2>&1; then
+        log_debug "Getting VM description..."
         local desc="$(
             qm config "$vmid" | \
             grep -i '^description:' | \
             sed 's/description: //'
         )"
+        log_debug "Template info mode: ${mode}"
         case "$mode" in
             hash) result="$(echo "$desc" | \
                 grep -Eo '\b[a-f0-9]{64}\b|\b[a-f0-9]{128}\b'
@@ -117,14 +151,17 @@ get_template_info() {
             tag) result="$(echo "$desc" | \
                 grep -Eo 'v?[0-9]+(\.[0-9]+)+'
             )";;
-            *) echo "[WARN] Unsupported template get info mode"; return 1;;
+            *) log_warn "Unsupported get_template_info mode"; return 1;;
         esac
         if [ -n "$result" ]; then
+            log_info "Successfully read template info for vm ${vmid}"
             printf '%s' "$result"
         else
+            log_debug "Template info is empty"
             echo ""
         fi
     else
+        log_debug "Cannot get VM ${vmid} config details"
         echo ""
     fi
 }
@@ -133,39 +170,61 @@ fetch() {
   curl --silent --show-error --fail "$@"
 }
 
-#################################LOGS##########################################
+################################# LOGS ########################################
 
-# UTC timestamp
-__ts() {
-    date -u +"%Y-%m-%dT%H:%M:%SZ"
+_log() {
+    local level="$1"
+    local priority="$2"
+    shift 2
+    local msg="$*"
+    local ts
+
+    ts="$(date -Is)"
+
+    # UTC timestamps
+    # ts="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+
+    # File log
+    printf '%s [%s] %s\n' "$ts" "$level" "$msg" >>"$LOG_FILE"
+
+    # journald (native priority)
+    logger -t pve-templater -p "user.${priority}" "$msg"
 }
 
+log_debug() {
+    [[ "$DEBUG_ENABLED" -eq 1 ]] || return 0
+    _log DEBUG debug "$@"
+}
+log_info()    { _log INFO    info    "$@"; }
+log_notice()  { _log NOTICE  notice  "$@"; }
+log_warn()    { _log WARN    warning "$@"; }
+log_error()   { _log ERROR   err     "$@"; }
+log_crit()    { _log CRIT    crit    "$@"; }
+
 rotate_logs() {
+    log_debug "Checking if log rotation is needed"
     if [[ -f "$LOG_FILE" ]]; then
         local size
+        log_debug "Log file ${LOG_FILE} exists"
         size=$(stat -c%s "$LOG_FILE")
+        log_debug "Log filesize: ${size}"
         if (( size > "$MAX_LOG_SIZE" )); then
+            log_debug "Log exceeds 1MB. Rotating the log..."
             mv "$LOG_FILE" "${LOG_FILE}.$(date -u +%Y%m%d%H%M%S)"
+            log_debug "Creating new empty log file"
             touch "$LOG_FILE"
-            log_info "Log rotated due to size >1MB"
+            log_notice "Log rotated due to size >1MB"
         fi
     fi
 }
 
-flush_logs() {
-    exec 300>>"$LOG_FILE"
-    flock 300
-    cat "$TMP_LOG" >&300
-    echo "[INFO] Finished flushing logs. Removing tmp file..."
-    rm -f "$TMP_LOG"
-}
-
-############################GITHUB#############################################
+############################### GITHUB ########################################
 
 get_latest_github() {
     require_bin curl
     local project="$1"
     local data
+    log_info "Getting project ${project} data from GitHub..."
     data="$(
         curl --silent --fail-with-body \
             -H "Accept: application/vnd.github+json" \
@@ -178,7 +237,9 @@ get_tag() {
     require_bin jq
     local data="$1"
     local tag
+    log_info "Parsing GitHub data to get latest tag..."
     tag="$(jq -er '.tag_name' <<<"$data")"
+    log_debug "Latest tag: ${tag}"
     printf '%s' "$tag"
 }
 
@@ -186,43 +247,107 @@ get_published_at() {
     require_bin jq
     local data="$1"
     local published_at
+    log_info "Parsing GitHub data to get published date..."
     published_at="$(jq -er '.published_at' <<<"$data")"
+    log_debug "Published date: ${published_at}"
     printf '%s' "$published_at"
 }
 
-#################################IMAGES########################################
+################################ IMAGES #######################################
+
+# dl_image() {
+#     require_bin wget
+#     local image_url="$1"
+#     local hash
+#     local target_path
+#     mkdir -p "$CACHE_DIR"
+#     target_path="${CACHE_DIR%/}/$(basename "$image_url")"
+#     echo "[INFO] Downloading ${image_url}..."
+#     wget -q --show-progress -O "$target_path" "$image_url"
+#     hash="$(get_hash sha512 "$target_path")"
+#     echo "[INFO] SHA512 hashsum: ${hash}"
+# }
 
 dl_image() {
     require_bin wget
+
     local image_url="$1"
     local hash
     local target_path
-    mkdir -p "$CACHE_DIR"
+    local tmp_path
+
+    mkdir -p "$CACHE_DIR" || {
+        log_error "Failed to create cache directory: $CACHE_DIR"
+        return 1
+    }
+
     target_path="${CACHE_DIR%/}/$(basename "$image_url")"
-    echo "[INFO] Downloading ${image_url}..."
-    wget -q --show-progress -O "$target_path" "$image_url"
-    hash="$(get_hash sha512 "$target_path")"
-    echo "[INFO] SHA512 hashsum: ${hash}"
+    tmp_path="${target_path}.tmp.$$"
+
+    log_info "Downloading image: ${image_url}"
+    log_debug "Target path: ${target_path}"
+    log_debug "Temporary path: ${tmp_path}"
+
+    if ! wget \
+        -q --show-progress \
+        --timeout=30 \
+        --tries=3 \
+        --read-timeout=30 \
+        -O "$tmp_path" "$image_url"
+    then
+        log_error "Download failed: ${image_url}"
+        log_debug "Removing temporary file: ${tmp_path}"
+        rm -f "$tmp_path"
+        return 1
+    fi
+
+    if [ ! -s "$tmp_path" ]; then
+        log_error "Downloaded file is empty: ${image_url}"
+        log_debug "Removing temporary file: ${tmp_path}"
+        rm -f "$tmp_path"
+        return 1
+    fi
+
+    if ! hash="$(get_hash sha512 "$tmp_path")"; then
+        log_error "Failed to calculate SHA512 hash for file: ${tmp_path}"
+        log_debug "Removing temporary file: ${tmp_path}"
+        rm -f "$tmp_path"
+        return 1
+    fi
+
+    log_info "SHA512 checksum: ${hash}"
+
+    if ! mv -f "$tmp_path" "$target_path"; then
+        log_error "Failed to move file into cache: ${target_path}"
+        log_debug "Removing temporary file: ${tmp_path}"
+        rm -f "$tmp_path"
+        return 1
+    fi
+
+    log_info "Image cached successfully: ${target_path}"
 }
 
 get_hash() {
     local hash_type="$1"
     local file="$2"
+    log_debug "Creating SHA hash for file ${2}"
     case "$hash_type" in
         sha256) sha256sum "$file" | awk '{print $1}';;
         sha512) sha512sum "$file" | awk '{print $1}';;
-        *) echo "[WARN] Unsupported hash type"; return 1;;
+        *) log_warn "Unsupported hash type"; return 1;;
     esac
+    log_debug "Hash was successfully created"
 }
 
 inject_qemu() {
     require_bin virt-customize
     local image="$1"
-    echo "Injecting qemu-guest-agent"
+    log_info "Injecting qemu-guest-agent"
     virt-customize -a "$image" --install qemu-guest-agent
+    log_info "Success! qemu-guest-agent was installed into image ${image}"
 }
 
-###############################TEMPLATES#######################################
+############################## TEMPLATES ######################################
 
 talos() {
     require_bin jq
@@ -238,42 +363,52 @@ talos() {
     talos_url="https://factory.talos.dev/image/${TALOS_SCHEMATIC_ID}/${latest_tag}/nocloud-amd64.raw.xz"
     talos_image="${CACHE_DIR%/}/$(basename "${talos_url%.xz}")"
     if [[ -z "$latest_data" ]]; then
-        echo "[ERROR] failed to fetch latest GitHub release"
+        log_error "Failed to fetch latest GitHub release"
         return 1
     elif [[ -z "$latest_tag" ]]; then
-        echo "[ERROR] failed to parse latest tag"
+        log_error "Failed to parse latest tag"
         return 1
     elif [[ -z "$current_tag" ]]; then
-        echo "[WARN] failed to get current tag"
+        log_warn "Failed to get current tag"
     elif [[ "$current_tag" = "$latest_tag" ]]; then
-        echo "[INFO] Talos image already up-to-date. Exiting."
+        log_info "Talos image already up-to-date. Exiting."
         return 0
     fi
+    log_info "Downloading latest Talos image..."
+    log_debug "Talos URL: ${talos_url}"
+    log_debug "Talos tag: ${latest_tag}"
     dl_image "$talos_url"
+    log_debug "Unpacking Talos image..."
     xz -dfk "${CACHE_DIR%/}/$(basename "$talos_url")"
+    log_info "Creating new VM ${name} for Talos template"
     create_vm "$vmid" "$name"
+    log_info "Importing Talos image to VM ${vmid}"
     import_disk "$vmid" "$talos_image"
+    log_info "Writing description with Talos image details"
     qm set "$vmid" --description "$(cat <<EOF
 Talos Linux published at: ${latest_date}  
-Version: ${latest_tag}  
+Version: *${latest_tag}*  
 Date: $(date +"%a %b %d %H:%M:%S %Z %Y")
 EOF
 )"
-    echo "[INFO] Converting VM ${vmid} to template..."
+    log_info "Converting VM ${vmid} to template..."
     qm template "$vmid"
-    echo "Removing image ${talos_image}"
+    log_info "Deleting unpacked image ${talos_image} (archive is untouched)"
     rm -f "$talos_image"
 }
 
 ###############################################################################
 
 main() {
-    trap flush_logs EXIT
-#   rotate_logs
+    trap 'log_error "Command failed: \"$BASH_COMMAND\" (line $LINENO)"' ERR
+    get_lock
+    rotate_logs
+    log_info "=== pve-templater started (PID $$) ==="    
 #   parse_flags "$@"
 #   log_info "###############################################################################"
 #   log_info "Start time:" __ts
     talos "905" "talos-latest"
+    log_info "=== pve-templater finished successfully ==="
 }
 
 main "$@"
